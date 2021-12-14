520921732abcd6215110674df8a9019a9cb05b70
Nguyen Phuong <95663567+phuongnt9x@users.noreply.github.com>
2021-12-14 08:56:47 +0700

    color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { ColorComponent } from './color/color.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
